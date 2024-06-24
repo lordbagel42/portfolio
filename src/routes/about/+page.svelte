301c74cb45c8d16731650f796eed1ee0075992fb
Raygen Rupe <raygenrrupe@gmail.com>
2024-06-24 06:37:37 -0600

    cloudflare pages

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import '../../app.css';
+</script>
+
+<div class="flex justify-center items-center h-view">
+	<div class="text-center">
+		<p class="">hi</p>
+	</div>
+</div>
---
+++ src/routes/projects/bagelindustries/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../../../app.css';
+</script>
+
+<div class="flex justify-center items-center h-view">
+	<div class="w-1/2 flex justify-center items-center">
+		<p>left.</p>
+	</div>
+	<div class="w-1/2 flex justify-center items-center">
+		<p>right</p>
+	</div>
+</div>
